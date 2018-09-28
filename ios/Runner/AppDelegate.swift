2e1e46b4660bf8ebf6c212e7d522e0e535e9580a
zonble <zonble@gmail.com>
2018-09-29 00:43:37 +0800

    Adds MS App Center support on iOS.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,21 @@
 import UIKit
 import Flutter
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+	override func application(
+			_ application: UIApplication,
+			didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+	) -> Bool {
+		GeneratedPluginRegistrant.register(with: self)
+		MSAppCenter.start("8e5f593c-a6e4-4cc4-b924-3771666f5afe", withServices: [
+			MSAnalytics.self,
+			MSCrashes.self
+		])
+
+		return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+	}
 }
