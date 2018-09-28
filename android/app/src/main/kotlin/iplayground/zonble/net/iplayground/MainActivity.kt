84e672e4f1657a9562519a28f70ed5e018fd3e50
zonble <zonble@gmail.com>
2018-09-29 01:08:00 +0800

    Adds MS App Center support on Android.

--- android/app/src/main/kotlin/iplayground/zonble/net/iplayground/MainActivity.kt
+++ android/app/src/main/kotlin/iplayground/zonble/net/iplayground/MainActivity.kt
@@ -5,9 +5,17 @@ import android.os.Bundle
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity(): FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+
+class MainActivity() : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+        AppCenter.start(getApplication(), "661605e6-8c8b-48d4-be26-4e341d863f95",
+                Analytics::class.java, Crashes::class.java)
+    }
 }
